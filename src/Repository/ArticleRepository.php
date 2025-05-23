3901ac00127a90c911b8baf147d4ea033e627735
Pierre Bertrand <pierre.bertrand.professionnel@gmail.com>
2025-05-23 17:01:18 +0200

    feat(articles): add crud for article and pagination

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Dto\Filter\ArticleFilterDto;
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -16,7 +17,34 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-//    /**
+    public function countAll(): int
+    {
+        return $this->createQueryBuilder('a')
+            ->select('COUNT(a.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    public function findPaginate(ArticleFilterDto $filter): array
+    {
+        $offset = ($filter->getPage() - 1) * $filter->getLimit();
+
+        $query = $this->createQueryBuilder('a')
+            ->setMaxResults($filter->getLimit())
+            ->setFirstResult($offset);
+
+        $total = $this->countAll();
+
+        return [
+            'items' => $query->getQuery()->getResult(),
+            'meta' => [
+                'pages' => ceil($total / $filter->getLimit()),
+                'total' => $total,
+            ]
+        ];
+    }
+
+    //    /**
 //     * @return Article[] Returns an array of Article objects
 //     */
 //    public function findByExampleField($value): array
@@ -31,7 +59,7 @@ class ArticleRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?Article
+    //    public function findOneBySomeField($value): ?Article
 //    {
 //        return $this->createQueryBuilder('a')
 //            ->andWhere('a.exampleField = :val')
